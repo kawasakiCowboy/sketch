82f8dd24caecadadaac06acaf558183dc0cb51dc
kawasakiCowboy <gmuradian@yandex.ru>
2024-12-12 15:45:13 +0300

    added on click functionality

--- script.js
+++ script.js
@@ -9,17 +9,29 @@
 let container = document.querySelector(".container");
 let userInput = document.querySelector("#user-input");
 let userButton = document.querySelector("#user-button");
+let isClicked = false
 
 function pixelCreate(n) {
     for (let j = 1; j <= n; j++ ) {
     let pixelRow = document.createElement("div");
     pixelRow.classList.add("row");
+    pixelRow.setAttribute('draggable','false');
     for (let i = 1; i <= n; i++ ) {
         let pixel = document.createElement("div");
         pixel.classList.add("pixel");
+        pixel.setAttribute('draggable','false');
+        pixel.addEventListener("mousedown", () => {
+            isClicked = true;
+            event.preventDefault();
+        } )
+        pixel.addEventListener("mouseup", () => {
+            isClicked = false;
+            event.preventDefault();
+        } )
         pixel.addEventListener("mouseover", () => {
-            console.log("hover")
-            pixel.classList.add("pixel-active");
+            if (isClicked == true) {
+            pixel.classList.add("pixel-active");}
+            event.preventDefault();
         } )
         pixelRow.appendChild(pixel);
     }
